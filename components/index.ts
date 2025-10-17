6e721946fd5c56eec7acd37f393978e7d685fce3
Cornelius Asogwa <cornelius.webdev@gmail.com>
2025-10-17 19:33:23 +0100

    chore(components/index.ts): barrel-export component files

---
+++ components/index.ts
@@ -0,0 +1,17 @@
+import FeaturedProjectsSection from './sections/FeaturedProjectsSection';
+import HeroSection from './sections/HeroSection';
+import ContactSection from './sections/ContactSection';
+import SkillsSection from './sections/SkillsSection';
+import Header from './Header';
+import ProjectCard from './ProjectCard';
+import AboutSection from './sections/AboutSection';
+
+export {
+	FeaturedProjectsSection,
+	Header,
+	HeroSection,
+	SkillsSection,
+	ProjectCard,
+	ContactSection,
+	AboutSection,
+};
