18aa88bd3712543d3bd56f7ba50e97bd9b42aebc
Cornelius Asogwa <cornelius.webdev@gmail.com>
2025-10-17 10:47:18 +0100

    feat(public-assets): add public assets (documents/ and images/)

---
+++ public/images/index.ts
@@ -0,0 +1,5 @@
+import creditCard from './credit-card.jpg';
+import spaceTourism from './space-tourism.jpg';
+import profile from './me-blurred.jpg';
+
+export { creditCard, spaceTourism, profile };
