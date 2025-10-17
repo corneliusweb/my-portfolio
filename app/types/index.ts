ea0f8cf57791c0ca67893d727130b63a2e8e633c
Cornelius Asogwa <cornelius.webdev@gmail.com>
2025-10-17 19:28:17 +0100

    chore(types/index.ts): restore tracked file

---
+++ app/types/index.ts
@@ -0,0 +1,51 @@
+import { StaticImageData } from 'next/image';
+import type { IconType } from 'react-icons';
+
+export interface Project {
+	id: string;
+	name: string;
+	image: StaticImageData;
+	description: string;
+	techStacks: string[];
+	challenge?: string[];
+	addedFeatures?: string[];
+	solution?: string;
+	liveURL?: string;
+	githubURL?: string;
+	date?: string;
+	role?: string;
+}
+
+export interface Skill {
+	id: string;
+	name: string;
+	category: 'Core' | 'Framework' | 'Tool' | 'Library';
+	icon?: IconType | React.FC<CustomIconProps>;
+}
+
+export interface SocialLink {
+	id: string;
+	name: string;
+	href: string;
+	rel: string;
+	target?: string;
+	title: string;
+	'aria-label': string;
+	icon: IconType;
+}
+
+export interface SocialLinkProps {
+	show?: string[];
+	LinkClass?: string;
+	IconClass: string;
+	size?: number;
+}
+
+export type CustomIconProps = React.SVGProps<SVGSVGElement> & { size?: number };
+
+export interface ContactFormData {
+	name: string;
+	email: string;
+	subject: string;
+	message: string;
+}
