43e303158a99f44e5f9819ba811acf7259598a6e
Cornelius Asogwa <cornelius.webdev@gmail.com>
2025-11-05 21:51:25 +0100

    chore(config): enforce removal of console logs in production

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  compiler: {
+    // automatically remove console logs from production
+		removeConsole: process.env.NODE_ENV === 'production',
+	},
 };
 
 export default nextConfig;
