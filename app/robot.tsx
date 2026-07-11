bf3a6272fd3fa5978b95d54992e58e5d1560a8c5
Cornelius Asogwa <cornelius.webdev@gmail.com>
2026-07-11 19:58:16 +0100

    feat(robot): add sitemap and robot file

---
+++ app/robot.tsx
@@ -0,0 +1,16 @@
+import { BASE_URL } from "@/constants";
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+	return {
+		rules: [
+			{
+				userAgent: "*",
+				allow: "/",
+				disallow: ["/api/", "/_next/", "/private/"],
+			},
+		],
+		sitemap: `${BASE_URL}/sitemap.xml`,
+		host: BASE_URL,
+	};
+}
